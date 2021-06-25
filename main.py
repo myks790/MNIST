563c40fe7c6e8b27837f48b6658e105c0420f329
Kang Sang-hun <myks790@gmail.com>
2021-06-25 14:11:17 +0900

    학습된 모델로 이미지 인식 시도(실패)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
+import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 if __name__ == '__main__':
     mnist = tf.keras.datasets.mnist
@@ -15,8 +17,10 @@ if __name__ == '__main__':
     # 모델 층 쌓기
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28)),
+        # 입력층 생성
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(0.2),
+        # 출력층 생성
         tf.keras.layers.Dense(10, activation='softmax')
     ])
 
@@ -25,8 +29,20 @@ if __name__ == '__main__':
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    # 모델 훈련
+    # 모델 학습
     model.fit(x_train, y_train, epochs=5)
 
     # 모델 평가
     model.evaluate(x_test, y_test, verbose=2)
+
+    # 학습된 모델로 이미지 인식 해보기
+    im = cv2.imread('9img.png')
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  # 색공간 변환
+    im = cv2.resize(im, (28, 28))  # 사이즈 조정
+    plt.imshow(im, cmap='Greys', interpolation="nearest")
+    plt.show()
+    im = im.reshape(-1, 28, 28) / 255
+    print(im)
+    predictions = model.predict(im)
+    print(predictions)
+    print("예측한 결과 = ", np.argmax(predictions[0]))
